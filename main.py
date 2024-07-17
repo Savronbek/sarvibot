89d5820a3b635d6acf9bf87fb6e1516464e25d7d
Savronbek <savronbek098@gmail.com>
2024-07-17 05:11:04 +0300

    norm commit

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
 import logging
 from aiogram import Bot, Dispatcher, types
-from aiogram.dispatcher import executor
 from dotenv import load_dotenv
 from db import Database
 import markups as nav
+import asyncio
+from aiogram.filters import Command
 
 # Загрузка переменных окружения
 load_dotenv()
@@ -14,16 +15,16 @@ logging.basicConfig(level=logging.INFO)
 
 # Инициализация бота и диспетчера
 bot = Bot(token=os.getenv('TOKEN'))
-dp = Dispatcher(bot)
+dp = Dispatcher()
 db = Database('database.db')
 
 # Команда /start
-@dp.message_handler(commands=['start'])
+@dp.message(Command('start'))
 async def cmd_start(message: types.Message):
     if message.chat.type == 'private':
         start_command = message.text.strip()
         referrer_id = start_command[7:] if len(start_command) > 7 else None
-        
+
         if not db.user_exists(message.from_user.id):
             if referrer_id and referrer_id != str(message.from_user.id):
                 if db.add_user(message.from_user.id, referrer_id):
@@ -44,18 +45,18 @@ async def cmd_start(message: types.Message):
             await bot.send_message(message.from_user.id, 'Xush kelibsiz!', reply_markup=nav.mainMenu)
 
 # Обработка сообщения "профиль"
-@dp.message_handler()
+@dp.message()
 async def bot_message(message: types.Message):
     if message.text == 'Profil':
         referral_count = db.count_referrals(message.from_user.id)
         await bot.send_message(message.from_user.id, f"ID: {message.from_user.id}\nhttps://t.me/{os.getenv('BOT_NICKNAME')}?start={message.from_user.id}\nTo'plangan ballar soni: {referral_count}")
 
-# Асинхронная функция запуска бота
-async def on_startup(dp):
-    logging.info('Starting bot...')
-    await bot.set_webhook()  # Установка вебхука
-    logging.info('Bot started!')
-
 # Запуск бота
+async def main():
+    # Удаляем вебхук, если он был установлен
+    await bot.delete_webhook(drop_pending_updates=True)
+    # Запускаем polling
+    await dp.start_polling(bot)
+
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    asyncio.run(main())
